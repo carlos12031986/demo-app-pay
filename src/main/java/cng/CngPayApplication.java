a4ae3908bfa75d5c05d2e6ec506745bcea557fc9
rlf09360128 <90850166+rlf09360128@users.noreply.github.com>
2023-10-29 15:26:07 -0600

    ESto es el primer commit 2023-10-29

---
+++ src/main/java/cng/CngPayApplication.java
@@ -0,0 +1,13 @@
+package cng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CngPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CngPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cng/CngPayApplicationTests.java
@@ -0,0 +1,13 @@
+package cng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CngPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
